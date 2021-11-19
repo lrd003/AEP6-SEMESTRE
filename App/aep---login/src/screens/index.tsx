1cd0c162467b4b20ad7d15cd8bf9f43f72c8bb45
Leonardo de Oliveira Gonçalves <37309720+lrd003@users.noreply.github.com>
2021-11-18 22:04:34 -0300

    Error
    Error

--- App/aep---login/src/screens/index.tsx
+++
@@ -1,5 +0,0 @@
-export { default as LoginScreen } from './LoginScreen';
-export { default as RegisterScreen } from './RegisterScreen';
-export { default as ForgotPasswordScreen } from './ForgotPasswordScreen';
-export { default as Dashboard } from './Dashboard';
-export { default as List } from './List';
